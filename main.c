a4d5435822ce016a8a85c49cc0ca1a07b8982dbe
juicio8 <you@example.com>
2022-09-02 13:00:27 -0700

    initial commit

--- main.c
+++ main.c
@@ -1,24 +1,66 @@
-#include shell.h
+#include "shell.h"
 
-
+char *read_line();
+char **split_line(char *line);
 
 int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
-    while (1)
-    {
-        PRINT("$ ");
-        char *line = read_line();
-        PRINT(line);
+	char *line;
+	char **tokens;
 
-    }
+	while (1)
+	{
+		PRINT("$ ");
+		line = read_line();
+		tokens = split_line(line);
+		free(line);
+		free(tokens);
+	}
+	
 }
 
 
-char *read_line() {
-    char *line = NULL;
-    size_t buflen = 0;
+char *read_line() 
+{
+	char *line = NULL;
+	size_t buflen = 0;
 
-    getline(&line, &buflen, stdin);
-    return (line);
+	getline(&line, &buflen, stdin);
+	return (line);
+}
 
+#define DELIMETER " \n\t\r\a\v"
+char **split_line(char *line)
+{
+	int i = 0;
+	char **tokens;
+	char *token;
+	size_t capacity = 64;
+
+	tokens = malloc(capacity * sizeof(char));
+	if (tokens == NULL)
+	{
+		perror("There's an error");
+		return (-1);
+	}
+
+	token = strtok(line, DELIMETER);
+
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		PRINT(tokens[i]);
+		PRINT("\n");
+		i++;
+
+		if (i >= capacity)
+		{
+			tokens = realloc(tokens, (capacity * 2) * sizeof(char));
+		}
+
+		token = strtok(NULL, DELIMETER);
+	       	
+	}
+	tokens[i] = NULL;
+	return tokens;
 }
--- shell.h
+++ shell.h
@@ -1,6 +1,10 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#define PRINT(c) (write(STDOUT_FILENO, c,_strlen(c)));
+#define PRINT(c) (write(STDOUT_FILENO, c, strlen(c)));
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#endif
